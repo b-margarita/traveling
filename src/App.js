97e6742c3b0bdfd1af5dd26772766d589be8dfb3
b-margarita <q.bmargarita@gmail.com>
2023-08-29 12:35:09 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { data } from './data';
 
 function App() {
+  const [place, setPlace] = useState(0);
+  const {id, name, image, description} = data[place];
+  const backPlase = () => {
+    setPlace((place => {
+      place --;
+      if (place < 0) {
+        return data.length - 1;
+      }
+      return place;
+    }))
+  }
+  const nextPlace = () => {
+    setPlace((place => {
+      place ++;
+      if (place > data.length - 1) {
+        place = 0
+      }
+      return place;
+    }))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        <h1>{id} {name}</h1>
+      </div>
+      <div className='container'>
+        <img src={image} alt='place' width="500px"/>
+      </div>
+      <div className='container'>
+        <a href={description} target='_blank' rel="noopener noreferrer">description</a>
+      </div>
+      <div className='container btn'>
+        <button onClick={backPlase}>Back</button>
+        <button onClick={nextPlace}>Next</button>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+        id: 1,
+        name: "Petra",
+        image: "https://img.freepik.com/free-photo/closeup-shot-old-building-constructed-cliff-during-daytime_181624-10974.jpg?w=1380&t=st=1693296580~exp=1693297180~hmac=7d2d4f43f6cfb401b11d4686d79e77f17106b19a115f57971b6c06575ad9f0cc",
+        description: "https://www.nationalgeographic.com/history/article/lost-city-petra" 
+    },
+    {
+        id: 2,
+        name: "Machu Picchu",
+        image: "https://img.freepik.com/free-photo/bird-s-eye-view-breathtaking-mountain-machu-picchu-peru_181624-10826.jpg?w=1060&t=st=1693297362~exp=1693297962~hmac=996939447febf980ec59b4ca1bdec5a6b2a7e14515068337861f1ab32b799471",
+        description: "https://www.nationalgeographic.com/history/article/machu-picchu-mystery" 
+    },
+    {
+        id: 3,
+        name: "Grand Canyon",
+        image: "https://img.freepik.com/free-photo/sunrise-mesa-arch-canyonlands-national-park-near-moab-utah-usa_268835-1016.jpg?w=1060&t=st=1693297425~exp=1693298025~hmac=d5299f806301410ac54c181baf2b0b2e63662d1b2ce1e1e9f2f4f330fa225d79",
+        description: "https://www.nationalgeographic.com/travel/national-parks/article/grand-canyon-national-park" 
+    },
+    {
+        id: 4,
+        name: "Florentia",
+        image: "https://img.freepik.com/free-photo/beautiful-aerial-shot-florence-italy-architecture-evening_181624-2203.jpg?w=1060&t=st=1693297642~exp=1693298242~hmac=66c052b020e0d1a042de9cddc6ebb8fb00bb80e7f0b940d2973a5774443f2e45",
+        description: "https://www.nationalgeographic.co.uk/travel/2015/03/city-life-florence" 
+    },
+    {
+        id: 5,
+        name: "Amsterdam",
+        image: "https://img.freepik.com/free-photo/beautiful-shot-bicycles-leaned-again-fence-bridge-river_181624-3802.jpg?w=1060&t=st=1693297817~exp=1693298417~hmac=2c5e017a10f04385ce66dbacdc5654a626ff1526a570fb4d02d86dee07ebb4dc",
+        description: "https://www.nationalgeographic.com/travel/article/fast-facts-78" 
+    },
+    {
+        id: 6,
+        name: "Geirangerfjorden",
+        image: "https://img.freepik.com/free-photo/bird-eye-shot-view-geirangerfjord-norway_181624-29054.jpg?w=1060&t=st=1693298021~exp=1693298621~hmac=a5acb2ebcb641db1bd3d9a06349a5cd3d87f8754c38b2c5ac0d82321f80e63db",
+        description: "https://education.nationalgeographic.org/resource/fjord/" 
+    }
+]
